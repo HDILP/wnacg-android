27a7e758be499a19a0918a17634622a5b9559db3
晗菌 <awm31415@163.com>
2026-08-28 20:13:32 +0800

    fix: add missing android.content.Intent import in MainActivity

--- android/app/src/main/java/com/wnacg/android/MainActivity.java
+++ android/app/src/main/java/com/wnacg/android/MainActivity.java
@@ -16,6 +16,7 @@ import java.io.InputStreamReader;
 import java.io.FileOutputStream;
 
 import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
