2b5da75e724b78f5635d73f8a834ae22af8985a1
晗菌 <awm31415@163.com>
2026-08-29 20:35:45 +0800

    fix(gui): 补 ArrayAdapter import (Spinner 适配器编译错误)

--- android/app/src/main/java/com/wnacg/android/MainActivity.java
+++ android/app/src/main/java/com/wnacg/android/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
